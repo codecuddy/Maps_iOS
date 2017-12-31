ad518cb15123600cb1866a54fed2e0f3442548b9
Charlie <charliecuddy4@gmail.com>
2017-12-31 15:15:00 -0600

    first use of maps in iOS

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 40.7127431
+        
+        let longitude: CLLocationDegrees = -74.0133795
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let longDelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        // let span: MKCoordinateSpanMake(latDelta, longDelta)  --> shortcut don't need to specify type
+        
+        let coordinates: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegionMake(coordinates, span)
+        
+        map.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "One World Trade"
+        
+        annotation.subtitle = "I want to visit!"
+        
+        annotation.coordinate = coordinates
+        
+        map.addAnnotation(annotation)
+        
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+    }
+    
+    @objc func longPress(gestureRecognizer: UIGestureRecognizer) {
+        let touchPoint = gestureRecognizer.location(in: self.map)
+        
+        let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = coordinate
+        
+        annotation.title = "New Place"
+        
+        annotation.subtitle = "Should I go here?"
+        
+        map.addAnnotation(annotation)
     }
 
     override func didReceiveMemoryWarning() {
